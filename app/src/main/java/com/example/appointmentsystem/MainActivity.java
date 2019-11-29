3dce3d31c821b9b55fe787c93a082454f3db01b0
MSI\User <enricojulian@gmail.com>
2019-11-29 10:29:51 +0800

    Merge branch 'master' of https://github.com/aqlhzlkfl/AppointmentSystem
    # Conflicts:
    #	.idea/vcs.xml

--- app/src/main/java/com/example/appointmentsystem/MainActivity.java
+++ app/src/main/java/com/example/appointmentsystem/MainActivity.java
@@ -5,13 +5,49 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText ETLEmail;
+    EditText ETLPass;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+        ETLEmail = (EditText)findViewById(R.id.ETLEmail);
+        ETLPass = (EditText)findViewById(R.id.ETLPass);
+    }
+
+    public void login(View view){
+        String email = ETLEmail.getText().toString();
+        String pass = ETLPass.getText().toString();
+        boolean result = false;
+        if (email.equals("dev")){
+            result = true;
+            Intent goToHome = new Intent(MainActivity.this, Home.class);
+            startActivity(goToHome);
+        }
+        else if (!email.isEmpty() && !pass.isEmpty()){
+
+            result = myDb.checkUser(email, pass);
+            if (result == true){
+                Intent goToHome = new Intent(MainActivity.this, Home.class);
+                startActivity(goToHome);
+            }
+            else{
+                Toast.makeText(MainActivity.this,"Email or Password is incorrect", Toast.LENGTH_LONG).show();
+            }
+        }
+        if (result == false){
+            Toast.makeText(MainActivity.this,"Fill in the form", Toast.LENGTH_LONG).show();
+        }
+
     }
 
     public void goToRegister(View view){
--- app/src/main/java/com/example/appointmentsystem/Register.java
+++ app/src/main/java/com/example/appointmentsystem/Register.java
@@ -5,13 +5,63 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+    EditText ETFirstN;
+    EditText ETLastN;
+    EditText ETEmail;
+    EditText ETPass;
+    EditText ETConfPass;
+    RadioButton RBMale;
+    RadioButton RBFemale;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        myDb = new DatabaseHelper(this);
+        ETFirstN = (EditText)findViewById(R.id.ETFirstName);
+        ETLastN = (EditText)findViewById(R.id.ETLastName);
+        ETEmail = (EditText)findViewById(R.id.ETemail);
+        ETPass = (EditText)findViewById(R.id.ETpassword);
+        ETConfPass = (EditText)findViewById(R.id.ETconfpassword);
+        RBMale = (RadioButton) findViewById(R.id.RBMale);
+        RBFemale = (RadioButton) findViewById(R.id.RBFemale);
+        register = (Button)findViewById(R.id.btnRegister);
+
+    }
+
+    public void register(View view){
+        String pass = ETPass.getText().toString();
+        String confpass = ETConfPass.getText().toString();
+        boolean result = false;
+        if (pass.equals(confpass)){
+            String gender = "";
+            if (RBMale.isChecked()){
+                gender = "male";
+            }
+            if (RBFemale.isChecked()){
+                gender = "female";
+            }
+            result = myDb.saveUser(ETFirstN.getText().toString(), ETLastN.getText().toString(), ETEmail.getText().toString(), pass, gender);
+
+            if (result == true){
+                Intent goToLog = new Intent(Register.this, MainActivity.class);
+                Toast.makeText(Register.this,"User registered successfully",Toast.LENGTH_LONG).show();
+                startActivity(goToLog);
+            }
+        }
+        if (result == false){
+            Toast.makeText(Register.this,"Confirm password must match with password",Toast.LENGTH_LONG).show();
+        }
+
     }
 
     public void goToLogin(View view){
