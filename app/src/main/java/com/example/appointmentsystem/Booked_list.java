004988a61bebe7f3619a28d0ea32709685801cce
Muhd Mujib <muhdiqmal1997@gmail.com>
2019-12-16 22:49:57 +0800

    I add the appointment form activity

---
+++ app/src/main/java/com/example/appointmentsystem/Booked_list.java
@@ -0,0 +1,14 @@
+package com.example.appointmentsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Booked_list extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_booked_list2);
+    }
+}
